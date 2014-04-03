16d0da0117618f06ea116e51a2b11ca5eefe07da
Leo Holsti <lholsti@Leo-Holstis-MacBook-Pro.local>
2014-04-03 20:34:29 +0300

    Small changes to plotter. Switched to using BlooleanClause.Occur.SHOULD instead of MUST. This gives us all ranked results, instead of only those which satisfy the boolean query. Previously we were getting 20-40 results, even though the topics limit was set to 1000. I set the limit to 50, for testing purposes.

--- src/ir_course/LuceneSearchApp.java
+++ src/ir_course/LuceneSearchApp.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -20,6 +21,8 @@ import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.*;
 import org.apache.lucene.search.*;
+import org.apache.lucene.search.FieldComparator.RelevanceComparator;
+import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.search.similarities.DefaultSimilarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -30,7 +33,10 @@ public class LuceneSearchApp {
     public static final String INDEXFILE = "index";
     public static final String TITLE = "title";
     private static final String ABSTRACT = "abstract";
+    public static final String RELEVANT = "relevant";  
 
+    private static int totalNumRelevantRecords = 0;
+    
     public LuceneSearchApp() {
 
     }
@@ -56,11 +62,15 @@ public class LuceneSearchApp {
             List<String> results;
 
             /* Queries found in corpus:
-             automatic face recognition
-             computer vision analysis
-             image pattern recognition
-             scene analysis
+             automatic face recognition, relevant documents: 50
+             computer vision analysis, relevant documents: 21
+             image pattern recognition, relevant documents: 37
+             scene analysis, relevant documents: 32
+             	sum total relevant documents = 140
              */
+            
+            totalNumRelevantRecords = 140;
+            
             inAbstract = new ArrayList<>();
             inAbstract.add("automatically");
             results = engine.search(null, null, inAbstract, null);
@@ -107,7 +117,7 @@ public class LuceneSearchApp {
             IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_42, analyzer);
             if (isTfIdf) {
                 // DefaultSimilarity is subclass of TFIDFSimilarity
-                iwc.setSimilarity(new DefaultSimilarity());
+            	iwc.setSimilarity(new DefaultSimilarity());
             }
             iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
             IndexWriter w = new IndexWriter(dir, iwc);
@@ -116,6 +126,11 @@ public class LuceneSearchApp {
                 Document doc = new Document();
                 doc.add(new Field(TITLE, documentInCollection.getTitle(), TextField.TYPE_STORED));
                 doc.add(new Field(ABSTRACT, documentInCollection.getAbstractText(), TextField.TYPE_STORED));
+                if(documentInCollection.isRelevant() && documentInCollection.getSearchTaskNumber() == 4) {
+                	doc.add(new Field(RELEVANT, "true", TextField.TYPE_STORED));
+                } else {
+                	doc.add(new Field(RELEVANT, "false", TextField.TYPE_STORED));
+                }
                 w.addDocument(doc);
             }
             w.close();
@@ -141,7 +156,7 @@ public class LuceneSearchApp {
         BooleanQuery query = new BooleanQuery();
         addTermQueries(inTitle, query, TITLE, BooleanClause.Occur.MUST);
         addTermQueries(notInTitle, query, TITLE, BooleanClause.Occur.MUST_NOT);
-        addTermQueries(inAbstract, query, ABSTRACT, BooleanClause.Occur.MUST);
+        addTermQueries(inAbstract, query, ABSTRACT, BooleanClause.Occur.SHOULD);
         addTermQueries(notInAbstract, query, ABSTRACT, BooleanClause.Occur.MUST_NOT);
 
         return collectResults(searcher, query);
@@ -149,18 +164,29 @@ public class LuceneSearchApp {
 
     private List<String> collectResults(IndexSearcher searcher, BooleanQuery query) {
         List<String> results = new LinkedList<String>();
-        TopScoreDocCollector collector = TopScoreDocCollector.create(1000, true);
+        TopScoreDocCollector collector = TopScoreDocCollector.create(50, true);
         try {
             // DefaultSimilarity is subclass of TFIDFSimilarity
             DefaultSimilarity similarity = new DefaultSimilarity();
             searcher.setSimilarity(similarity);
             searcher.search(query, collector);
             ScoreDoc[] hits = collector.topDocs().scoreDocs;
+            LinkedList<Document> plotlist = new LinkedList<Document>(); 
             for (ScoreDoc sdoc : hits) {
                 int docId = sdoc.doc;
                 Document d = searcher.doc(docId);
-                results.add(d.get(TITLE));
+                results.add(d.get(TITLE) );
+                plotlist.add(d);
             }
+            /* Run plotter
+             * Plotter takes lucene documents, so it must be called from here
+             * before the results are converted to List<String> and relevance data is lost.
+             * Plotter also needs to know what the total recall is for each query, as the results
+             * don't include those relevant documents that the query missed.
+             */ 
+            Plotter plotter = new Plotter("/tmp/");
+            String texplot = plotter.PlotListAsString(plotlist, "testplot", totalNumRelevantRecords);
+            System.out.print(texplot);
 
         } catch (IOException e) {
             System.err.println("Error collecting results!");
--- src/ir_course/Plotter.java
+++ src/ir_course/Plotter.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import org.apache.lucene.document.*;
 
 public class Plotter {
 	private String basedir ="";
@@ -31,10 +32,10 @@ public class Plotter {
 
     
     /* write pr-curve as to basedir/PlotTitle.tex */
-    public boolean PlotListToFile(List<DocumentInCollection> docs, String PlotTitle) {
+    public boolean PlotListToFile(List<Document> docs, String PlotTitle, int totalRecall) {
     	try {
     		FileWriter fw = new FileWriter(PlotTitle+".tex", false);
-    		fw.write(PlotListAsString(docs,PlotTitle));
+    		fw.write(PlotListAsString(docs,PlotTitle, totalRecall));
     		fw.close();
     	} catch (IOException e) {
     		e.printStackTrace();
@@ -44,27 +45,27 @@ public class Plotter {
     }
 
     /* return pr-curve as string */
-    public String PlotListAsString(List<DocumentInCollection> docs, String plotTitle) {
+    public String PlotListAsString(List<Document> docs, String plotTitle, int totalRecall) {
     	String plotTex = "\\begin{tikzpicture} \n\\begin{axis}[\n\ttitle={";
     	plotTex += plotTitle;
     	plotTex += "},\n\txlabel={Recall}, \n\tylabel={Precision},\n\txmin=0, \n\txmax=1,\n\tymin=0, \n\tymax=1,"
     			+ "\n\txtick={0,0.2,0.4,0.6,0.8,1.0},"
     			+ "\n\tytick={0,0.2,0.4,0.6,0.8,1.0},"
     			+ "\n\tlegend pos=outer north east,]";
-    	plotTex += "\n\\addplot["
+    	plotTex += "\n\\addplot[color=blue,mark=*,]"
     			+"\n\tcoordinates {";
-    	List<Point> curvepoints = countPR(docs);
+    	List<Point> curvepoints = countPR(docs, totalRecall);
     	for(Point p : curvepoints) {
-    		plotTex += "C"+p.x +","+p.y+")";
+    		plotTex += "("+p.x +","+p.y+") ";
     	}
-    	plotTex += "\nend{axis} \nend{tikzpicture}";
+    	plotTex += "};\n\\end{axis} \n\\end{tikzpicture}";
     	
     	return plotTex;
     }
     
-    private List<Point> countPR(List<DocumentInCollection> docs) {
+    private List<Point> countPR(List<Document> docs, int totalRecall) {
     	List<Point> curvepoints = new LinkedList<Point>();
-    	for(int i=0; i<10; i++) {
+    	for(int i=0; i<11; i++) {
     		curvepoints.add(new Point((double)i/10, 0.0));
     	}
     	
@@ -72,8 +73,8 @@ public class Plotter {
     	int[] relevantCount = new int[docs.size()];
     	
     	for(int i = 0; i< docs.size(); i++) {
-    		DocumentInCollection d = docs.get(i);
-    		if(d.isRelevant() == true && d.getSearchTaskNumber() == 4) {
+    		Document d = docs.get(i);
+    		if(d.get(LuceneSearchApp.RELEVANT).toString().equalsIgnoreCase("true")) {
     			relevantTotal++;
     			relevantCount[i] = relevantTotal;
     		}
@@ -85,9 +86,11 @@ public class Plotter {
     	nextPoint.y = 1.0;
     	nextPoint = pointIterator.next();
     	for(int i=1; i < relevantCount.length; i++) {
-    		if(relevantCount[i]/relevantTotal >= nextPoint.x) {
-    			nextPoint.y = (relevantCount[i]/i);
-    			nextPoint = pointIterator.next();
+    		if((double)relevantCount[i]/totalRecall >= nextPoint.x) {
+    			nextPoint.y = ((double)relevantCount[i]/(i+1));
+    			if(pointIterator.hasNext()) {
+    				nextPoint = pointIterator.next();
+    			}
     		}
     	}
     	
